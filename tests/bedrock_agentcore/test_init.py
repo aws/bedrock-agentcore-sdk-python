0eab997b8f0f3e29f38ceed7dea9b243c9366d6e
T.J Ariyawansa <tjariy@amazon.com>
2026-02-03 16:01:00 -0500

    refactor: remove Project class
    Remove Project resource registry class to simplify the SDK.
    Agent and Memory classes can be used independently.

--- tests/bedrock_agentcore/test_init.py
+++ tests/bedrock_agentcore/test_init.py
@@ -24,7 +24,6 @@ def test_all_exports():
     expected_all = [
         "BedrockAgentCoreApp",
         "PingStatus",
-        "Project",
         "RequestContext",
         "BedrockAgentCoreContext",
     ]
